fe2ed381c809526d7669d3fb46e6ec9f410ce983
Martin Rodríguez <neomartinr@gmail.com>
2021-10-19 17:06:05 -0300

    Starting project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Router, NavLink } from 'react-router-dom';
+import Home from './pages/home/home';
+import Contact from './pages/contact/contact';
+import About from './pages/about/about';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <NavLink to="/" activeClassName="active-link" exact>Home</NavLink>
+        <NavLink to="/contact" activeClassName="active-link" exact>Contacto</NavLink>
+        <NavLink to="/about" activeClassName="active-link" exact>Acerca de</NavLink>
+        <NavLink to="/login" activeClassName="active-link" exact>Login</NavLink>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/login" exact component={Login}></Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+    </>
   );
 }
 
